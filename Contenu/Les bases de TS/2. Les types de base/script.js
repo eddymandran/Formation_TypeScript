4e100254a0785301fb5f9efafb65bc24fbc40eee
eddymandran <eddy.mandran@gmail.com>
2022-10-20 14:29:58 +0200

    Les types de bases

---
+++ Contenu/Les bases de TS/2. Les types de base/script.js
@@ -0,0 +1,14 @@
+"use strict";
+let str = "str";
+let num = 5;
+let array = [];
+let object = {
+    a: 5
+};
+let toogle = true;
+let anything;
+let randomNumber;
+const conversion = (celsius) => {
+    return (celsius * 9 / 5) + 32;
+};
+console.log(conversion(10));
